7be1cb9ecd16169686d5733c2662e7b05347501a
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-03-13 14:43:32 +0100

    Update years in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from meteo import get_data
 
 def main():
     url = "https://www.historique-meteo.net/afrique/cote-d-ivoire"
-    years = [2022, 2021, 2020]
+    years = [2024, 2022, 2021]
     df = get_data(url, years)
 
     return df
