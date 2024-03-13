3ffc88280c0874571cd2fea17599362680de3f25
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-03-13 14:25:16 +0100

    Update years in get_data function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from meteo import get_data
-from sqlalchemy import create_engine
 
 
 def main():
     url = "https://www.historique-meteo.net/afrique/cote-d-ivoire"
-    years = [2022]
+    years = [2022, 2021, 2020]
     df = get_data(url, years)
 
     return df
@@ -12,5 +11,4 @@ def main():
 
 if __name__ == "__main__":
     df = main()
-    engine = create_engine("sqlite:///output/meteo_data_civ.db", echo=False)
-    df.to_sql("meteo_data_civ", con=engine, if_exists="replace", index=False)
+    df.to_csv("output/meteo.csv", index=False)
