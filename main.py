e87879264f80b8c35cac9419b76fa4ef3191feeb
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-08-26 20:40:24 +0200

    Code to install all the data from 2020 to 2025

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from meteo import get_data
 
 def main():
     url = "https://www.historique-meteo.net/afrique/cote-d-ivoire/"
-    #years = list(range(2020, 2025))
-    years = [2024]
+    years = list(range(2020, 2025))
+    #years = [2024]
     df = get_data(url, years)
 
     return df
@@ -14,3 +14,7 @@ if __name__ == "__main__":
     data = main()
     data.to_parquet("meteo.parquet", index=False)
     print(data)
+
+
+
+
